ffca41c95e8d87b07ea11fbed57f67e9f5fb67af
Paul Tim <paveltimofeenko@gmail.com>
2021-01-13 10:05:36 +0300

    add input validation

--- NotixyBotApp/Models/Commands/CovidCommand.cs
+++ NotixyBotApp/Models/Commands/CovidCommand.cs
@@ -16,13 +16,14 @@ namespace NotixyBotApp.Models.Commands
             string info;
 
             var country = message.Text.Replace("/covid ", "");
-            if(country=="")
+
+            if (country == "/covid")
             {
                 info = "Please enter country after /covid command";
             }
             else
-            info = GetCovid(country);
-
+                info = GetCovid(country);
+            
             await client.SendTextMessageAsync(chatId, info, replyToMessageId: messageId);
         }
 
@@ -34,6 +35,9 @@ namespace NotixyBotApp.Models.Commands
             string jsonData = client.DownloadString(url);
 
             var details = JObject.Parse(jsonData);
+           
+            if(details["country"].ToString().ToLower() != country.ToLower())
+                return "Please enter valid country";
 
             string covidCountry = string.Concat("Results for : ", details["country"]);
             string covidTotalCases = string.Concat("Total cases : ", details["cases"]);
@@ -43,7 +47,7 @@ namespace NotixyBotApp.Models.Commands
             string covidTodayCases = string.Concat("Today cases : ", details["todayCases"]);
             string covidTodayDeaths = string.Concat("Today deaths : ", details["todayDeaths"]);
 
-            string covidData = $"{covidCountry}\n{covidTotalCases}\n{covidTotalActive}\n{covidTotalRecovered}\n{covidTotalDeaths}\n{covidTodayCases}\n{covidTodayDeaths}";
+            string covidData = $"❗\n{covidCountry}\n{covidTotalCases}\n{covidTotalActive}\n{covidTotalRecovered}\n{covidTotalDeaths}\n{covidTodayCases}\n{covidTodayDeaths}";
             return covidData;
         }
     }
